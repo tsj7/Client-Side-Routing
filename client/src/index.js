460058c9c48cd571b6c9e9efc9961be3b21c6b0f
Tyge Johnson <tygesjohnson@gmail.com>
2018-11-15 13:48:56 -0700

    Update index.js
    edit movies list component

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import MovieList from './Movies/MovieList';
+import MovieList from './Movies/MovieList'; 
 import Movie from './Movies/Movie'
 
 ReactDOM.render(
